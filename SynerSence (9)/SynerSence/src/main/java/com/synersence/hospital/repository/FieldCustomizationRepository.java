307614fa74cf91d285077c4272ad2eabb16a78b4
Pankti0911 <pankti112004@gmail.com>
2025-12-30 21:41:02 +0530

    Delete SynerSence (9)/SynerSence directory

--- SynerSence (9)/SynerSence/src/main/java/com/synersence/hospital/repository/FieldCustomizationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.synersence.hospital.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.synersence.hospital.entity.FieldCustomization;
-
-public interface FieldCustomizationRepository
-        extends JpaRepository<FieldCustomization, Long> {
-}
